982f51bb21b818d8d78b83e8530fc8ea5efbb098
AhmedMadian <ahmed.mostafa.madian@gmail.com>
2020-01-13 02:36:49 +0200

    Add radio button functionality

--- NewsApp/NewsApp/Presentation/Modules/Filter/View/FilterViewController.swift
+++ NewsApp/NewsApp/Presentation/Modules/Filter/View/FilterViewController.swift
@@ -17,6 +17,10 @@ class FilterViewController: BaseViewController {
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var exitButton: UIButton!
     @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var selectCountryButton: UIButton!
+    @IBOutlet weak var selectSourceButton: UIButton!
+    @IBOutlet weak var countryRadioButton: UIButton!
+    @IBOutlet weak var sourceRadioButton: UIButton!
     
     //MARK:- Properties
     var viewModel: FilterViewModel!
@@ -25,11 +29,47 @@ class FilterViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         containerView.makeRoundedCorners(with: 20)
+        countryRadioButton.isSelected = true
+        selectCountryButton.isEnabled = true
+        viewsInititalState()
+    }
+    
+    func viewsInititalState() {
+        selectSourceButton.isEnabled = false
+        
     }
     
     //MARK:- IBAction
     @IBAction func cancel_Tapped(_ sende: UIButton) {
         viewModel.dismiss()
+    }
+    @IBAction func selectCountryDidTapped(_ sender: Any) {
+        print("Country")
+    }
+    @IBAction func selectSourceDidTapped(_ sender: Any) {
+        print("Source")
+    }
+    @IBAction func filterDidTapped(_ sender: Any) {
+    }
+    @IBAction func countryRadioDidTapped(_ sender: UIButton) {
+        togglInRadio(firstButton: sender, secondButton: sourceRadioButton)
+        selectCountryButton.isEnabled = sender.isSelected
+        selectSourceButton.isEnabled = !sender.isSelected
+    }
+    @IBAction func sourceRadioDidTapped(_ sender: UIButton) {
+        togglInRadio(firstButton: sender, secondButton: countryRadioButton)
+        selectSourceButton.isEnabled = sender.isSelected
+        selectCountryButton.isEnabled = !sender.isSelected
+    }
+    
+    func togglInRadio(firstButton: UIButton, secondButton: UIButton) {
+        if firstButton.isSelected {
+            firstButton.isSelected = false
+            secondButton.isSelected = false
+        }else {
+            firstButton.isSelected = true
+            secondButton.isSelected = false
+        }
     }
     
 }
